a6873f97b2aaccf96a0e98bfc33a5d9639536ecb
RIZWAN <RIZWAN@FACULTY18>
2019-04-10 20:16:14 +0500

    RoleBasedLogin

---
+++ WebRoleBasedLogin/WebRoleBasedLogin/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using WebRoleBasedLogin.Models;
+namespace WebRoleBasedLogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        OnlineMallEntities _ome = new OnlineMallEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+            int _flag = _ome.Users.Where(m => m.UserName == user.UserName && m.UsePassword == user.UsePassword).Count();
+         int _roleid=   _ome.Users.Where(m => m.UserName == user.UserName && m.UsePassword == user.UsePassword).Single().UserRole.Role_Id;
+            if (_flag > 0) {
+
+                FormsAuthentication.SetAuthCookie(user.UserName, false);
+                switch (_roleid)
+                {
+                    case 1:
+                        return RedirectToAction("Index", "Admin");
+                        break;
+                    case 2:
+                        return RedirectToAction("Index", "User");
+                        break;
+                    default:
+                        break;
+                }
+
+               
+            }
+            return View();
+        }
+
+        public ActionResult LogOut() {
+
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+    }
+}
---
+++ WebRoleBasedLogin/WebRoleBasedLogin/Models/CustomRole.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using WebRoleBasedLogin.Models;
+namespace WebRoleBasedLogin.Models
+{
+    public class CustomRole : RoleProvider
+    {
+        OnlineMallEntities ome = new OnlineMallEntities();
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetRolesForUser(string username)
+        {
+           String s = ome.Users.Where(x => x.UserName == username).Single().UserRole.RoleName;
+            String[] _data = new string[] {s};
+            return _data;
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
