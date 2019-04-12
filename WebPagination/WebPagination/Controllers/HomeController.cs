aacf947c55eb981c416a5dfce839f0e78f240556
RIZWAN <RIZWAN@FACULTY18>
2019-04-12 18:05:36 +0500

    Pagination Example

---
+++ WebPagination/WebPagination/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebPagination.Models;
+using PagedList;
+using System.Threading.Tasks;
+
+namespace WebPagination.Controllers
+{
+    public class HomeController : Controller
+    {
+        productdbEntities pdEntity = new productdbEntities();
+        
+        // GET: Home
+        public async Task<ActionResult> Index(int page =1,int pageSize=3)
+        {
+            List<PRODUCT> _Product = pdEntity.PRODUCTs.ToList();
+            PagedList<PRODUCT> model = new PagedList<PRODUCT>(_Product,page,pageSize);
+            return View(model);
+        }
+    }
+}
